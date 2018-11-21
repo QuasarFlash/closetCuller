a1340d55d91c1ca60265f067738dfc2145250715
daftclaud <cdsmileyone@gmail.com>
2018-11-21 12:02:29 -0500

    adds homepage progress

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  constructor(private router: Router, private authSvc: AuthService) {}
+
+  go(type) {
+    this.router.navigateByUrl(`tabs/(home:home/${type})`);
+  }
+
+  logout() {
+    this.authSvc.logout().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private authSvc: AuthService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authSvc.login();
   }
 
 }
