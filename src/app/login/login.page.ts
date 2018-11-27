c77ffa34cc838818b3bbd915f67fc5117e3b7190
Chris Kasper <chriskasper@Chriss-MacBook-Pro.local>
2018-11-26 22:21:37 -0500

    some login page edits

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,17 +4,25 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+
+  username:string;
+  password:string;
 
   constructor(private authSvc: AuthService) { }
 
   ngOnInit() {
   }
 
-  login() {
+  loginGoogle() {
     this.authSvc.login();
+  }
+
+  login() {
+    console.log("Username " + this.username);
+    console.log("Password " + this.password);
   }
 
 }
