be48a5ed6f738f43a19be6792ad8330ac4c32115
QuasarFlash <44853097+QuasarFlash@users.noreply.github.com>
2018-12-06 13:26:48 -0500

    Update Suggestions

--- src/app/Suggestions/Suggestions.page.ts
+++ src/app/Suggestions/Suggestions.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClosetService } from '../closet.service';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-Suggestions',
@@ -9,48 +11,76 @@ import { ClosetService } from '../closet.service';
 export class SuggestionsPage implements OnInit {
   time: string;
   total: number;
-  day:  string = 'day';
+  day: string = 'day';
   week: string = 'week';
-  month:  string = 'month';
-  year:  string = 'year';
+  month: string = 'month';
+  year: string = 'year';
   type: string;
-  items$: any;
+  images$: any;
   DateNow: any = new Date();
 
   constructor(
     private closetSvc: ClosetService) {
-      this.type = 'tshirt';
+    this.type = 'tshirt';
   }
   ngOnInit() {
     console.log('show suggestions');
     this.time = "day";
     this.total = 0;
     this.displayItems(Event);
+
   }
   displayItems($event) {
-    this.items$ = this.closetSvc.getItems(this.type);
+    console.log("HEre");
+    this.images$ = this.closetSvc.getItems(this.type).pipe(
+      map(items => {
+        return items.filter((item: any) => {
+          console.log(this.total);
+          var CapDate = new Date(item.last_worn);
+          if((this.time === this.day) && this.getDays(CapDate) >= this.total){
+            return true;
+          }
+          else if((this.time == this.week) && this.getWeeks(CapDate) >= this.total){
+            return true;
+
+          }
+          else if((this.time == this.month) && this.getMonths(CapDate) >= this.total){
+            return true;
+
+          }
+          else if((this.time == this.year) && this.getYears(CapDate) >= this.total){
+            return true;
+
+          }
+        })
+      })
+    );
   }
-  TimeRangeItems(imgdate){
-    
-    var full_time = (imgdate.seconds * 1000) + imgdate.nanoseconds/1000;
+
+  TimeRangeItems(imgdate) {
+    var full_time = (imgdate);
 
     var CaptureDate = new Date(full_time);
+    console.log(CaptureDate);
     var result: any;
-    
-    if (this.time == this.day){
-      result = this.getDays(full_time) + " day(s) ago.";
+
+    if (this.time == this.day) {
+      result = this.getDays(CaptureDate) + " day(s) ago.";
     }
-    else if (this.time == this.week){
-      result = this.getWeeks(full_time) + " week(s) ago.";
+    else if (this.time == this.week) {
+      result = this.getWeeks(CaptureDate) + " week(s) ago.";
     }
-    else if (this.time == this.month){
+    else if (this.time == this.month) {
       result = this.getMonths(CaptureDate) + " month(s) ago.";
     }
-    else if(this.time == this.year){
+    else if (this.time == this.year) {
       result = this.getYears(CaptureDate) + " year(s) ago.";
     }
     return result;
   }
+
+
+
   getDays(CapDate) {
     let days = (this.DateNow - CapDate) / (1000 * 60 * 60 * 24);
     days = Math.floor(days);
@@ -62,11 +92,12 @@ export class SuggestionsPage implements OnInit {
     return weeks;
   }
   getMonths(CapDate) {
-    let diffMonth = (this.DateNow.getFullYear() - CapDate.getFullYear())*12 + (this.DateNow.getMonth() - CapDate.getMonth());
+    let diffMonth = (this.DateNow.getFullYear() - CapDate.getFullYear()) * 12 + (this.DateNow.getMonth() - CapDate.getMonth());
     return diffMonth;
   }
   getYears(CapDate) {
     let diffyear = this.DateNow.getFullYear() - CapDate.getFullYear();
     return diffyear;
   }
+
 }
